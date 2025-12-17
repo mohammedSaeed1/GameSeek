d48e37b5fd9cfaf0ba1f6d1b16607ac1e1dd1a69
mohammedSaeed1 <mohamedseso2002@gmail.com>
2025-12-17 10:57:56 +0200

    feat: Add Main img in Navbar component

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,9 +1,11 @@
 import {NavLink } from 'react-router-dom';
 import mainLogo from '../assets/logo-sm.png';
+import wraper from '../assets/wraper.png';
 
 export default function Navbar() {
   return  <>
-   <nav className="bg-[#3A497B] p-4 flex justify-between items-center w-3/4 mx-auto -mt-9 rounded-2xl">
+    <img src={wraper} alt="game img" className='opacity-100 object-cover bg-[#2A2E33] w-full h-62.5'/>
+   <nav className="bg-[#3A497B] p-4 flex justify-between items-center w-3/4 mx-auto -mt-9 rounded-2xl shadow-2xl">
        <div className='flex items-center gap-x-2'>
          <img src={mainLogo} alt="mainLogo GameSeek" className='w-10' />
          <h1 className='text-white text-2xl'>GameSeek</h1>
@@ -15,6 +17,7 @@ export default function Navbar() {
             <NavLink to={'/permadeath'}><li className="text-white">permadeath</li></NavLink>
             <NavLink to={'/superhero'}><li className="text-white">superhero</li></NavLink>
             <NavLink to={'/pixel'}><li className="text-white">pixel</li></NavLink>
+           
         </ul>
 
     </nav>
