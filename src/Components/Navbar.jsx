e266cbce8b2853e6f5431322b513343e75f1d918
mohammedSaeed1 <mohamedseso2002@gmail.com>
2025-12-17 20:06:42 +0200

    Update: Edit GameSeek size on small screens

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -23,7 +23,7 @@ export default function Navbar() {
               alt="GameSeek Logo"
               className="w-8 h-8 sm:w-12 sm:h-12 group-hover:scale-110 transition-transform"
             />
-            <h1 className="text-white sm:text-3xl font-bold tracking-wider">
+            <h1 className="text-white text-2xl sm:text-3xl font-bold tracking-wider">
               Game<span className="text-blue-400">Seek</span>
             </h1>
           </div>
