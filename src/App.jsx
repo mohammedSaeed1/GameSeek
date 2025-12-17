34f57552363853b67d5b11a73fb9e6a44739d628
mohammedSaeed1 <mohamedseso2002@gmail.com>
2025-12-17 10:27:19 +0200

    feat: Create BrowserRouter in APP component with main path

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import SpecificCategory from './Components/SpecificCategory';
+
+const router = createBrowserRouter([{
+  path: "/" , index: true, element: <SpecificCategory/>
+}])
+
 export default function App() {
   return  <>
-   
+    <RouterProvider router={router}/>  
   </>
 }
 
