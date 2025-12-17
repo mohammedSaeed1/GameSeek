b152e51cc9112d31ea98b95f0183c017646ae2d4
mohammedSaeed1 <mohamedseso2002@gmail.com>
2025-12-17 20:08:07 +0200

    Update: Edit resonsive for Layout component on small screens

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -1,11 +1,10 @@
 import { Outlet } from 'react-router-dom'
 import Navbar from './Navbar'
-import SpecificCategory from './SpecificCategory'
 
 export default function Layout() {
   return <>
       <Navbar/>
-      <section className='w-[85%] mx-auto'>
+      <section className='sm:w-[85%] mx-auto'>
       <Outlet/>
       </section>
   </>
